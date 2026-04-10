c1e2eb7a02d302fa36d624ce9bcf8416605f19a5
Vu Phan <vu.phan.se@gmail.com>
2026-04-10 21:25:05 +0700

    feat: export rehydrateRepo from public api

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,7 @@
 // src/lib/index.ts
 export { indexRepo, getCachedIndex } from "./indexer.js";
+export { rehydrateRepo } from "./rehydrate.js";
+export type { RehydrateOptions, RehydrateResult } from "./rehydrate.js";
 export { RepoIdentityError, IndexError } from "./models.js";
 export type {
 	RepoCache,
